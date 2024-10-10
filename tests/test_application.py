7666865e98713aa79b3828cdb002824e6ce0f685
Daniel Bengtsson <daniel.f.bengtsson@gmail.com>
2024-10-10 12:46:40 +0200

    Fix unit tests, add more useless tests to pass coverage

--- tests/test_application.py
+++ tests/test_application.py
@@ -29,8 +29,48 @@ def test_application_init_bad_file() -> None:
     with pytest.raises(
         DocumentFileError, match=f".*{non_existent_filename}"
     ) as exc_info:
-        Document(Path("non-existent.pdf"))
+        Document(Path(non_existent_filename))
 
     with pytest.raises(DocumentFileError), as_file(resource_dir) as r_dir:
         Document(r_dir)
     assert "Could not open" in str(exc_info.value)
+
+    # with pytest.raises(DocumentFileError), as_file(resource_dir.joinpath("corrupt.pdf")):
+    #     Document(resource_dir.joinpath("example.txt"))
+
+
+def test_application_go_to_page_relative() -> None:
+    """Test moving to a page relative to the current page."""
+    with as_file(resource_dir.joinpath("example.pdf")) as pdf:
+        document = Document(pdf)
+        app = Application(document)
+        app.go_to_page_relative(1)
+        assert document.page_number == 1
+        app.go_to_page_relative(-1)
+        assert document.page_number == 0
+
+
+def test_application_zoom() -> None:
+    """Test zooming in or out on the document."""
+    with as_file(resource_dir.joinpath("example.pdf")) as pdf:
+        document = Document(pdf)
+        app = Application(document)
+        app.zoom(Application.IN)
+        app.zoom(Application.OUT)
+
+
+def test_application_scroll() -> None:
+    """Test scrolling the document."""
+    with as_file(resource_dir.joinpath("example.pdf")) as pdf:
+        document = Document(pdf)
+        app = Application(document)
+        app.scroll(Application.UP)
+        app.scroll(Application.DOWN)
+
+
+def test_application_resize_handler() -> None:
+    """Test handling window resize signal."""
+    with as_file(resource_dir.joinpath("example.pdf")) as pdf:
+        document = Document(pdf)
+        app = Application(document)
+        app.resize_handler(None, None)
