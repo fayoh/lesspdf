79e89b425ba8f2427a4d2c20e26ed000f3155129
Daniel Bengtsson <daniel.f.bengtsson@gmail.com>
2024-10-12 10:21:04 +0200

    Extract image from document

--- src/lesspdf/document.py
+++ src/lesspdf/document.py
@@ -1,9 +1,15 @@
 """Class for handling the document object."""
 
+from functools import cached_property
 from pathlib import Path
 
+import PIL
+import PIL.Image
 import pymupdf  # type: ignore
 from pymupdf import FileDataError  # type: ignore
+
+# TODO: page_number should be a property with a setter that checks the value
+# against the number of pages in the document.
 
 
 class Document:
@@ -22,6 +28,42 @@ class Document:
             self._document = pymupdf.open(str(self.file))
         except FileDataError as e:
             raise DocumentFileError(str(self.file)) from e
+
+    @cached_property
+    def num_pages(self) -> int:
+        """Return the number of pages in the document."""
+        return len(self._document)
+
+    def get_page_image(self) -> PIL.Image.Image:
+        """Return an image of the current page.
+
+        Returns:
+            PIL.Image.Image: The image of the current page.
+        """
+        # TODO: Only generate the image if the page number or zoom level has changed. (delete the property when changing any settings)
+        # Bit of a hack to get the page image without exporting a pixmap
+        # and then converting it to an image and opening in pillow.
+        # Comes from a pr to pymupdf that hasn't been merged yet
+        #
+        # https://github.com/pymupdf/PyMuPDF/pull/3911
+        new_pixmap = self._document.get_page_pixmap(pno=self.page_number)
+        colorspace = new_pixmap.colorspace
+        if colorspace is None:
+            mode = "L"
+        elif colorspace.n == 1:
+            mode = "L" if new_pixmap.alpha == 0 else "LA"
+        elif colorspace.n == 3:
+            mode = "RGB" if new_pixmap.alpha == 0 else "RGBA"
+        else:
+            mode = "CMYK"
+
+        self.page_image = PIL.Image.frombytes(
+            mode,
+            (new_pixmap.width, new_pixmap.height),
+            new_pixmap.samples,
+        )
+
+        return self.page_image
 
 
 class DocumentFileError(Exception):
--- tests/test_document.py
+++ tests/test_document.py
@@ -16,9 +16,11 @@ def test_document_initialization_with_valid_file(tmp_path: Path) -> None:
     with as_file(resource_dir.joinpath("example.pdf")) as pdf:
         doc = Document(pdf)
 
+    expected_num_pages = 4
     # Assertions
     assert doc.file == pdf
     assert doc.page_number == 0
+    assert doc.num_pages == expected_num_pages
 
 
 def test_document_initialization_with_invalid_file() -> None:
@@ -26,3 +28,13 @@ def test_document_initialization_with_invalid_file() -> None:
     # Expect DocumentFileError to be raised
     with as_file(resource_dir.joinpath("corrupt.pdf")) as pdf, pytest.raises(DocumentFileError) as exc_info:
         Document(pdf)
+
+
+def test_get_page_image() -> None:
+    """Test getting an image of a page."""
+    with as_file(resource_dir.joinpath("example.pdf")) as pdf:
+        doc = Document(pdf)
+        image = doc.get_page_image()
+        assert image is not None
+        assert image.width > 0
+        assert image.height > 0
